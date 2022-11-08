218578f265bcddebc2d86c9e41b03d347311d092
gtgambit <gtgambit@gmail.com>
2022-11-08 15:44:43 +0200

    news

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,5 @@
 //Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
-
+//
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
